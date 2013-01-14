4bda69683ed22f471350130ec2203c04a4d08cd6
Andreas Persson <andreas_g.persson@maxlab.lu.se>
2013-01-14 12:56:50 +0100

    add PointGrey camera device

---
+++ camera/PointGrey.py
@@ -0,0 +1,164 @@
+############################################################################
+# This file is part of LImA, a Library for Image Acquisition
+#
+# Copyright (C) : 2009-2011
+# European Synchrotron Radiation Facility
+# BP 220, Grenoble 38043
+# FRANCE
+#
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+############################################################################
+#=============================================================================
+#
+# file :        PointGrey.py
+#
+# description : Python source for the PointGrey and its commands.
+#                The class is derived from Device. It represents the
+#                CORBA servant object which will be accessed from the
+#                network. All commands which can be executed on the
+#                Pilatus are implemented in this file.
+#
+# project :     TANGO Device Server
+#
+# copyleft :    MAX IV Laboratory
+#               Lund, Sweden
+#
+#=============================================================================
+#         (c) - Bliss - ESRF
+#=============================================================================
+#
+import PyTango
+from Lima import Core
+from Lima import PointGrey as PointGreyAcq
+from AttrHelper import get_attr_4u, get_attr_string_value_list
+
+
+class PointGrey(PyTango.Device_4Impl):
+
+    Core.DEB_CLASS(Core.DebModApplication, 'LimaCCDs')
+
+#------------------------------------------------------------------
+#    Device constructor
+#------------------------------------------------------------------
+    def __init__(self,*args) :
+        PyTango.Device_4Impl.__init__(self,*args)
+
+        self.init_device()
+
+        self.__Attribute2FunctionBase = { 'gain'          : 'Gain'
+                                        , 'auto_gain'     : 'AutoGain'
+                                        , 'exp_time'      : 'ExpTime'
+                                        , 'auto_exp_time' : 'AutoExpTime'
+                                        }
+
+#------------------------------------------------------------------
+#    Device destructor
+#------------------------------------------------------------------
+    def delete_device(self):
+        pass
+
+#------------------------------------------------------------------
+#    Device initialization
+#------------------------------------------------------------------
+    @Core.DEB_MEMBER_FUNCT
+    def init_device(self):
+        self.set_state(PyTango.DevState.ON)
+        self.get_device_properties(self.get_device_class())
+
+#------------------------------------------------------------------
+#    getAttrStringValueList command:
+#
+#    Description: return a list of authorized values if any
+#    argout: DevVarStringArray   
+#------------------------------------------------------------------
+    @Core.DEB_MEMBER_FUNCT
+    def getAttrStringValueList(self, attr_name):
+        #use AttrHelper
+        return get_attr_string_value_list(self, attr_name)
+    
+#==================================================================
+#
+#    PointGrey read/write attribute methods
+#
+#==================================================================
+    def __getattr__(self,name) :
+        #use AttrHelper
+        return get_attr_4u(self, name, _PointGreyCam)
+
+
+#==================================================================
+#
+#    PointGreyClass class definition
+#
+#==================================================================
+class PointGreyClass(PyTango.DeviceClass):
+
+    class_property_list = {}
+
+    device_property_list = {
+        'cam_ip_address':
+        [PyTango.DevString,
+         "Camera ip address",[]],
+        'cam_serial_number':
+        [PyTango.DevLong,
+         "Camera serial number",0],                            
+        }
+
+    cmd_list = {
+        'getAttrStringValueList':
+        [[PyTango.DevString, "Attribute name"],
+         [PyTango.DevVarStringArray, "Authorized String value list"]],
+        }
+
+    attr_list = {
+        'gain':
+        [[PyTango.DevDouble,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE]],
+        'auto_gain':
+        [[PyTango.DevBoolean,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE]],
+        'exp_time':
+        [[PyTango.DevDouble,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE]],
+        'auto_exp_time':
+        [[PyTango.DevBoolean,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE]],
+        }
+
+    def __init__(self,name) :
+        PyTango.DeviceClass.__init__(self,name)
+        self.set_type(name)
+
+#----------------------------------------------------------------------------
+# Plugins
+#----------------------------------------------------------------------------
+_PointGreyCam = None
+_PointGreyInterface = None
+
+def get_control(cam_ip_address = "", cam_serial_number = 0) :
+    global _PointGreyCam
+    global _PointGreyInterface
+
+    if _PointGreyCam is None:
+        _PointGreyCam = PointGreyAcq.Camera(cam_ip_address, int(cam_serial_number))
+        _PointGreyInterface = PointGreyAcq.Interface(_PointGreyCam)
+            
+    return Core.CtControl(_PointGreyInterface)
+
+def get_tango_specific_class_n_device():
+    return PointGreyClass, PointGrey
